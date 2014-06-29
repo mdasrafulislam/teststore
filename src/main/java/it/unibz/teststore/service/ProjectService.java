31a51a89d20d63fb1a3f894e5a0eda6d2d85ea23
mdasrafulislam <mdasraful.islam@gmail.com>
2014-06-29 18:31:38 +0200

    Build service added

--- src/main/java/it/unibz/teststore/service/ProjectService.java
+++ src/main/java/it/unibz/teststore/service/ProjectService.java
@@ -1,8 +1,6 @@
 package it.unibz.teststore.service;
 
-import it.unibz.teststore.entity.Blog;
 import it.unibz.teststore.entity.Project;
-import it.unibz.teststore.entity.User;
 import it.unibz.teststore.repository.ProjectRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
